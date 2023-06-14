91cae6e4cf4469c259d9efa0a91b887bbc42afd9
Tirth <tirthpatel2992003@gamail.com>
2023-06-15 03:10:06 +0530

    collection working

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,54 +1,65 @@
 import React from 'react'
 import { useState } from 'react';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth' 
-import {auth,storage} from '../firebase'
+import {auth,storage,db} from '../firebase'
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore"; 
+import { useNavigate, Link } from "react-router-dom";
+
 
 
 export const Register = () => {
+  const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
+  // const navigate = useNavigate();
 
-  const [err,setErr] = useState(false)
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
 
-  const handleSubmit = async (e)=>{
-    e.preventDefault()
-    const displayName = e.target[0].value
-    const email = e.target[1].value
-    const password = e.target[2].value
-    const file = e.target[3].files[0]
-    // console.log(e.target[0].value)
+    try {
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-  try{
-    const res = await createUserWithEmailAndPassword(auth, email, password)
-    const storageRef = ref(storage, displayName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-    
-    uploadTask.on(
-    (error) => {
-      setErr(true);
-    },
-    () => {
-      getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) => {
-        await updateProfile(res.user,{
-          displayName,
-          photoURL:downloadURL,
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            // navigate("/");
+          } catch (err) {
+            console.log(err);
+            setErr(true);
+            setLoading(false);
+          }
         });
-        await setDoc(db,"users",res.user.uid),{
-          uid:res.user.uid,
-          displayName,
-          email,
-          photoURL:downloadURL
-        }
       });
+    } catch (err) {
+      setErr(true);
+      setLoading(false);
     }
-  );
-
-
-}catch(err){
-    setErr(true);
-  }
-
-  }
+  };
   return (
     <div className='formContainer'>
         <div className="formWrapper">
