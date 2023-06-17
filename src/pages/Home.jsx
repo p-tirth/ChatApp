a3d5845bb26fd746aa5a9cf83954a35ae4e39b69
Tirth <tirthpatel2992003@gamail.com>
2023-06-17 16:18:41 +0530

    import fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { Login } from './Login'
 import { Chat } from '../components/Chat'
-import { Chats } from '../components/Chats'
-import Input from '../components/Input'
-import Message from '../components/Message'
-import Navbar from '../components/Navbar'
-import Search from '../components/Search'
 import { Sidebar } from '../components/sidebar'
 
 
