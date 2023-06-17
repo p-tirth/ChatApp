2ee90ec34dfdb2f275151246de61574ae8635fb0
Tirth <tirthpatel2992003@gamail.com>
2023-06-17 16:12:00 +0530

    userSearch

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import mona from '../img/mona.jpg'
+import { AuthContext } from '../context/AuthContext'
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from '../firebase';
 
 export const Chats = () => {
+  const [chats,setChats] = useState([])
+  const {currentUser} = useContext(AuthContext) 
+  useEffect(()=>{
+    const getChats = () =>{
+
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data())
+      });
+      return () =>{
+        unsub();
+      }
+    };
+
+    currentUser.uid && getChats()
+    },[currentUser.uid])
+    console.log(Object.entries(chats))
   return (
     <div className='chats'>
-      <div className="userChat">
-          <img src={mona} alt="" />
-          <div className="userChatInfo">
-            <span>mona</span>
-            <p>Hello</p>
-          </div>
+      {Object.entries(chats)?.map(chat=>(
+        <div className="userChat" key={chat[0]}>
+        <img src={chat[1].userInfo.photoURL} alt="" />
+        <div className="userChatInfo">
+          <span>{chat[1].userInfo.displayName}</span>
+          <p>{chat[1].userInfo.lastMessage?.text}</p>
         </div>
-        <div className="userChat">
-          <img src={mona} alt="" />
-          <div className="userChatInfo">
-            <span>mona</span>
-            <p>hi again</p>
-          </div>
-        </div>
+      </div>
+      ))}
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,75 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import mona from '../img/mona.jpg'
+import {AuthContext} from '../context/AuthContext'
+import { collection, query, doc, getDoc, getDocs, setDoc, where, updateDoc, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase'
+
 const Search = () => {
+  const [username,setUsername] = useState("")
+  const [user,setUser] = useState(null)
+  const [err,setErr] = useState(false)
+
+  const {currentUser} = useContext(AuthContext) 
+
+  const handleSearch = async () =>{
+    const q = query(collection(db,"users"),where("displayName","==",username));
+    try{
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach( (doc) => {setUser(doc.data())} );
+    }catch(err){
+      setErr(true);
+      console.log(err)
+    }
+  }
+
+  const handleKey = e=>{
+    e.code === "Enter" && handleSearch();
+  }
+  const handleSelect = async () =>{
+    const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid
+    try{
+      console.log(combinedId)
+      const res = await getDoc(doc(db,"chats",combinedId))
+      if(!res.exists()){
+        console.log("?")
+        await setDoc(doc(db,"chats",combinedId),{messages:[]});
+        await updateDoc(doc(db,"userChats",currentUser.uid),{
+          [combinedId+".userInfo"]:{
+            uid:user.uid,
+            displayName: user.displayName,
+            photoURL:user.photoURL
+          },
+          [combinedId+".date"]:serverTimestamp()
+        })
+        await updateDoc(doc(db,"userChats",user.uid),{
+          [combinedId+".userInfo"]:{
+            uid:currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL:currentUser.photoURL
+          },
+          [combinedId+".date"]:serverTimestamp()
+        })
+      }
+    }catch(err){
+      console.log(err)
+    }
+    setUser(null)
+    setUsername("")
+  }
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='Search people'/>
-        <div className="userChat">
-          <img src={mona} alt="" />
+        <input type="text" placeholder='Search people' value={username} onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)}/>
+        {err && <span>User not found!</span>}
+        {user && <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
           <div className="userChatInfo">
-            <span>mona</span>
+            <span>{user.displayName}</span>
           </div>
-        </div>
+        </div>}
       </div>
     </div>
   )
-}
+  }
 
 export default Search
