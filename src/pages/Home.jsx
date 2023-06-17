6778b19d5b04366012fbb9b28b3effdec4faafbc
Tirth <tirthpatel2992003@gamail.com>
2023-06-17 16:53:21 +0530

    relative import fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import { Login } from './Login'
-// import { Chat } from '../components/chat'
-// import { Chats } from '../components/chats'
-// import Input from '../components/Input'
-// import Message from '../components/Message'
-// import Navbar from '../components/navbar'
-// import Search from '../components/Search'
-// import { Sidebar } from '../components/sidebar'
-
+import { Login } from './Login'
+import { Chat } from 'src/components/chat'
+import { Chats } from 'src/components/chats'
+import Input from 'src/components/Input'
+import Message from 'src/components/Message'
+import Navbar from 'src/components/navbar'
+import Search from 'src/components/Search'
+import { Sidebar } from 'src/components/sidebar'
+import {Sidebar} from "src/components/Sidebar.jsx"
 
 export default function Home() {
   return (
